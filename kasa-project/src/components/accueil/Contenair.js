567389621b45a62541b5ee016ad6e93e736b37ad
Marie57412 <mariebraun1@outlook.fr>
2023-08-17 17:23:33 +0200

    css page accueil

--- kasa-project/src/components/accueil/Contenair.js
+++ kasa-project/src/components/accueil/Contenair.js
@@ -1,12 +1,25 @@
 import React from "react";
+import Logement from "../../data/logements.json";
 
 function Contenair() {
   return (
     <section className="contenair-card">
+      {Logement.map((item) => (
+        <div className="card" key={item.id}> {/* Clé déplacée ici */}
+          <div className="filter-card" />
+          <div className="title-card">
+            <h2>{item.title}</h2>
+            <p>{item.location}</p>
+          </div>
 
-      <div className="card">
-        
-      </div>
+          <img src={item.cover} alt={item.title} />
+          <div className="pictures">
+            {item.pictures.map((picture, index) => (
+              <img key={index} src={picture} alt={`Picture ${index + 1}`} />
+            ))}
+          </div>
+        </div>
+      ))}
     </section>
   );
 }
